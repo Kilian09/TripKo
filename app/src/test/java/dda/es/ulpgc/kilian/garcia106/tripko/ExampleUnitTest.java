4e588058462cb5600f4a6bfb4c150eba7bf9fa5d
Kilian <kilian.garcia106@alu.ulpgc>
2021-03-17 09:56:50 +0000

    Initial commit

---
+++ app/src/test/java/dda/es/ulpgc/kilian/garcia106/tripko/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package dda.es.ulpgc.kilian.garcia106.tripko;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
