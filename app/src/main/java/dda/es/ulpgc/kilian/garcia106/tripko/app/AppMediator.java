d4001300398d4a0f705e2eab559ff954b5e007ef
Gema del Toro <gema.del102@alu.ulpgc.es>
2021-03-30 14:45:58 +0100

    Creacion del AppMediator

--- app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/app/AppMediator.java
+++ app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/app/AppMediator.java
@@ -1,4 +1,45 @@
 package dda.es.ulpgc.kilian.garcia106.tripko.app;
 
-class AppMediator {
+public class AppMediator {
+
+    private static AppMediator INSTANCE;
+
+    private AppMediatorState mAppMediatorState;
+
+    private AppMediator() {
+        mAppMediatorState = new AppMediatorState();
+    }
+
+    public static void resetInstance() {
+        INSTANCE = null;
+    }
+
+
+    public static AppMediator getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new AppMediator();
+        }
+
+        return INSTANCE;
+    }
+
+    public AppMediatorState getAppMediatorState() {
+        return mAppMediatorState;
+    }
+
+    public NextToAppMediatorState getNextAppMediatorScreenState() {
+        return null;
+    }
+
+    public void setNextAppMediatorScreenState(AppMediatorToNextState state) {
+
+    }
+
+    public void setPreviousAppMediatorScreenState(AppMediatorToPreviousState state) {
+
+    }
+
+    public PreviousToAppMediatorState getPreviousAppMediatorScreenState() {
+        return null;
+    }
 }
