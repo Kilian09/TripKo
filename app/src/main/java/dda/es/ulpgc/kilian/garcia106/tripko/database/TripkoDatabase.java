88f260b0d99a78e48cd02e359e97a682f53e2094
Kilian <kilian.garcia106@alu.ulpgc>
2021-05-24 15:34:25 +0100

    Cambios en el TripkoDatabase para añadir el gastronomiaDao, platoDao y restauranteDao

--- app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/database/TripkoDatabase.java
+++ app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/database/TripkoDatabase.java
@@ -1,7 +1,16 @@
 package dda.es.ulpgc.kilian.garcia106.tripko.database;
 
+import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
+import dda.es.ulpgc.kilian.garcia106.tripko.data.GastronomiaItem;
+import dda.es.ulpgc.kilian.garcia106.tripko.data.PlatoItem;
+import dda.es.ulpgc.kilian.garcia106.tripko.data.RestauranteItem;
+
+@Database(entities = {GastronomiaItem.class, PlatoItem.class, RestauranteItem.class}, version = 1)
 public abstract class TripkoDatabase extends RoomDatabase {
 
+    public abstract GastronomiaDao gastronomiaDao();
+    public abstract PlatoDao platoDao();
+    public abstract RestauranteDao restauranteDao();
 }
