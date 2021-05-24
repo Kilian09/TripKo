54460d187fdc95dafe7c142867d6a37a9d8896c8
Kilian <kilian.garcia106@alu.ulpgc>
2021-05-24 15:14:58 +0100

    Cambios en los Items de gastronomia

--- app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/data/GastronomiaItem.java
+++ app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/data/GastronomiaItem.java
@@ -1,18 +1,27 @@
-package dda.es.ulpgc.kilian.garcia106.tripko.database;
+package dda.es.ulpgc.kilian.garcia106.tripko.data;
 
+import androidx.room.Entity;
 import androidx.room.Ignore;
 import androidx.room.PrimaryKey;
 
+import com.google.gson.annotations.SerializedName;
+
 import java.util.List;
 
+@Entity(tableName = "gastronomias")
 public class GastronomiaItem {
 
+    @PrimaryKey
     public int id;
 
     public String title;
 
-
+    @Ignore
+    @SerializedName("restauantes")
     public List<RestauranteItem> itemsRestaurante;
+
+    @Ignore
+    @SerializedName("platos")
     public List<PlatoItem> itemsPlato;
 
     @Override
---
+++ app/src/main/java/dda/es/ulpgc/kilian/garcia106/tripko/data/PlatoItem.java
@@ -0,0 +1,36 @@
+package dda.es.ulpgc.kilian.garcia106.tripko.data;
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.PrimaryKey;
+
+import static androidx.room.ForeignKey.CASCADE;
+
+@Entity(
+        tableName = "platos",
+        foreignKeys = @ForeignKey(
+                entity = GastronomiaItem.class,
+                parentColumns = "id",
+                childColumns = "gastronomia_id",
+                onDelete = CASCADE
+        )
+)
+public class PlatoItem {
+
+    @PrimaryKey
+    public int id;
+
+    public String title;
+    public String content;
+    public String picture;
+
+    @ColumnInfo(name = "gastronomia_id")
+    public int gastronomiaId;
+
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
